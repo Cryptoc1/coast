61cf0e0611e52bf2c3d25ee0cbcd6d260bd0d68c
Samuel Steele <steelesam72@gmail.com>
2016-04-16 14:53:23 -0400

    Missed a file

--- internal/js/internal-urls.js
+++ internal/js/internal-urls.js
@@ -1,7 +1,7 @@
 var internalURLs = [
     "coast:about",
-    "coast:downloads",
-    "coast:history",
+    // "coast:downloads",
+    // "coast:history",
     "coast:internal-urls",
     "coast:new-tab",
     "coast:settings"
